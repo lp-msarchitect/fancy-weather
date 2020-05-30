1fdc4bb24b692b0215e5ffeb4ed036fd86dfd3ba
Leonid Petrov <lp.msarchitect@gmail.com>
2020-05-31 00:37:41 +0300

    feat: add functions

--- fancy-weather/src/js/utils/clientApi.js
+++ fancy-weather/src/js/utils/clientApi.js
@@ -1,15 +1,36 @@
 const KEYS = {
   ymapsKey: '9bbb91ee-7d99-427e-b0ce-c301dbad09a5',
   weatherApiKey: 'da6962b372e64afbb47133333202705',
+  ipinfoKey: 'd5e7bd62571e27',
 };
+
+export async function getIpInfoPosition() {
+  const geo = await fetch(`https://ipinfo.io/json?token=${KEYS.ipinfoKey}`);
+  return await geo.json();
+}
 
 export async function getGeoObjectByCoords(coords) {
   if (!coords) return new Error('invalid coordinates');
   const data = await fetch(
-    `https://geocode-maps.yandex.ru/1.x/?lang=en_US&apikey=${KEYS.ymapsKey}&format=json&geocode=${coords.longitude},${coords.latitude}`
+    `https://geocode-maps.yandex.ru/1.x/?lang=en_US&apikey=${KEYS.ymapsKey}&kind=locality&format=json&geocode=${coords.longitude},${coords.latitude}`
   );
 
   return data.json();
+}
+
+export async function getGeoObjectByCityName(name) {
+  const data = await fetch(
+    `https://geocode-maps.yandex.ru/1.x/?lang=en_US&apikey=${KEYS.ymapsKey}&format=json&geocode=${name}`
+  );
+  return await data.json();
+}
+
+export async function getWeather(coords) {
+  const data = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=${KEYS.weatherApiKey}&q=${coords.latitude},${coords.longitude}&days=3`
+  );
+
+  return await data.json();
 }
 
 export async function getCurrentWeatherState(coords) {
